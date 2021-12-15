cd851ff577883a1e364661c789bc2d17f2c71f1a
ab268517 <ab268517@adalet.gov.tr>
2021-12-15 17:15:55 +0300

    docker compose, issue_id unique

--- src/main/java/ag/pinguin/issuetracker/entity/Bug.java
+++ src/main/java/ag/pinguin/issuetracker/entity/Bug.java
@@ -4,7 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -21,7 +31,7 @@ public class Bug implements Serializable {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "issue_id")
+    @Column(name = "issue_id", unique = true)
     private Long issueId;
 
     @Column(name = "title")
