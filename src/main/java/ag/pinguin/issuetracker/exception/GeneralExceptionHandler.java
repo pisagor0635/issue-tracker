815b89a532d322d43803f9674f249c1b2f58a6ad
ab268517 <ab268517@adalet.gov.tr>
2021-12-16 17:09:00 +0300

    work on logs

--- src/main/java/ag/pinguin/issuetracker/exception/GeneralExceptionHandler.java
+++ src/main/java/ag/pinguin/issuetracker/exception/GeneralExceptionHandler.java
@@ -1,20 +1,21 @@
 package ag.pinguin.issuetracker.exception;
 
-import ag.pinguin.issuetracker.logging.LogManager;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public final class GeneralExceptionHandler {
     private GeneralExceptionHandler() {
     }
 
     public static void handleException(Exception e) {
-        LogManager.getInstance().writeLog("An error occured : ", e);
+        log.error("An error occured : ", e);
     }
 
     public static void handleException(String message, Exception e) {
-        LogManager.getInstance().writeLog("An error occured" + message, e);
+        log.error("An error occured" + message, e);
     }
 
     public static void writeInfo(String message) {
-        LogManager.getInstance().writeInfoLog(message);
+        log.info(message);
     }
 }
